54aa3759b444afe5a0017bad5b95f0a755490b3a
Kyle <kderrick1988@gmail.com>
2019-03-03 10:29:50 -0800

    initial commit on basic initializr project

---
+++ src/main/java/com/kyle/springframework/spring5webapp/SpringwebappApplication.java
@@ -0,0 +1,13 @@
+package com.kyle.springframework.spring5webapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringwebappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringwebappApplication.class, args);
+	}
+
+}
