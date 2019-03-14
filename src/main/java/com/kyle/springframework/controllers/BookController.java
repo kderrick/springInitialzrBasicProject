884ebcc4e2bcaad97427b38ac428a3ea6226e0c8
Kyle <kderrick1988@gmail.com>
2019-03-14 16:40:46 -0700

    add BookControllerclass

---
+++ src/main/java/com/kyle/springframework/controllers/BookController.java
@@ -0,0 +1,28 @@
+package com.kyle.springframework.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.kyle.springframework.spring5webapp.BookRepository;
+
+@Controller
+public class BookController {
+
+	
+	private BookRepository bookRepo;
+	
+	public BookController(BookRepository bookRepo) {
+		this.bookRepo = bookRepo;
+	}
+	
+	@RequestMapping("/books")
+	public String getBooks(Model model) {
+		
+		model.addAttribute("books", bookRepo.findAll());
+		return "books";
+		
+	}
+
+
+}
